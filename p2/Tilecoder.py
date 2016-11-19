50e666f2f1a22332d084b72312bf40bd07f85deb
Aries0331 <jinzhu@ualberta.ca>
2016-11-19 00:05:48 -0700

    finish part1, change requirements

--- p2/Tilecoder.py
+++ p2/Tilecoder.py
@@ -2,15 +2,19 @@ numTilings = 8
     
 def tilecode(in1,in2,tileIndices):
     # write your tilecoder here (5 lines or so)
-    
+	for i in range(numTilings):
+		newX = int((in1+0.6/8*i)/0.6) # every time move 0.6/8, new location is (in1+0.6/8*i)
+		newY = int((in2+0.6/8*i)/0.6)
+		tileIndices[i] = newX+newY*11+121*i
+	return tileIndices
     
 def printTileCoderIndices(in1,in2):
-    tileIndices = [-1]*numTilings
+    tileIndices = [-1]*numTilings # create an array called tileIndices with initialization for eight elements all equal to -1
     tilecode(in1,in2,tileIndices)
-    print('Tile indices for input (',in1,',',in2,') are : ', tileIndices)
+    print('Tile indices for input (', in1, ',', in2,') are : ', tileIndices)
 
 printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
 printTileCoderIndices(5.99,5.99)
 printTileCoderIndices(4.0,2.1)
-    
+
